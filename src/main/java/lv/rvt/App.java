ca8bca133fd0e8762eeb1df957db4743d2837201
poleralph@gmail.com <ralfs.stabins2006@gmail.com>
2024-09-20 10:54:43 +0300

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,19 +8,27 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give speed:");
+        System.out.println("Give points [0-100]: ");
 
-        int speed = Integer.valueOf(scanner.nextLine());
-
-        if (speed > 120)
-        {
-            System.out.println("Speeding ticket!");
+        int points = Integer.valueOf(scanner.nextLine());
+        
+        if (points < 0) {
+            System.out.println("Grade: impossible");
+        } else if (points < 50) {
+            System.out.println("Grade: failed");
+        } else if (points < 60) {
+            System.out.println("Grade: 1");
+        } else if (points < 70) {
+            System.out.println("Grade: 2");
+        } else if (points < 80) {
+            System.out.println("Grade: 3");
+        } else if (points < 90) {
+            System.out.println("Grade: 4");
+        } else if (points <= 100) {
+            System.out.println("Grade: 5");
+        } else {
+            System.out.println("Grade: incredible");
         }
-        else
-        {
-            System.out.println("Speed is normal");
-        }
-
 
 
         scanner.close();

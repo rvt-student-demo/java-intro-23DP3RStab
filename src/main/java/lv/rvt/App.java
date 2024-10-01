ad670521a742d0610c47e3147315c7ef0ce70190
Ralfs <ralfs.stabins2006@gmail.com>
2024-10-01 18:34:11 +0300

    Counting

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,24 +8,11 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        int sum = 0;
-        int count = 0;
+        int n = Integer.valueOf(scanner.nextLine());
 
-        while (true) {
-            System.out.println("Give me a number: ");
-            int input = Integer.valueOf(scanner.nextLine());
-            
-            if (input != 0) {
-                sum = sum + input;
-                count = count + 1;
-                continue;
-            } else {
-                System.out.println("Number of numbers: " + count);
-                System.out.println("Sum of the numbers: " + sum);
-                break;
-            }
+        for (int i = 0; i < n+1; i++) {
+            System.out.println(i);
         }
-
 
         scanner.close();
     }

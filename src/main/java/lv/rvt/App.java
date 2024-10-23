427038a9ce24d6ca8b3fdd2f11cd0f7a9aaf4265
poleralph@gmail.com <ralfs.stabins2006@gmail.com>
2024-10-23 08:45:38 +0300

    Second plus third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,16 +1,30 @@
 package lv.rvt;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
+        Scanner numbers = new Scanner(System.in);
+        ArrayList<Integer> numberList = new ArrayList<>();
 
-        Stars.christmasTree(8);
+        System.out.println("ievadi ciparus atdalot ar ,");
+        String input = numbers.nextLine();
+        String[] parts = input.split(",");
 
-        scanner.close();
+        for (String part : parts) {
+            numberList.add(Integer.parseInt(part.trim()));
+        }
+        int sum = 0;
+        for (Integer i : numberList) {
+            sum += i;
+        }
+        System.out.println(numberList);
+        System.out.println("Summa: " + sum);
+
+        numbers.close();
     }
 
     public static void divisableByThreeInRange(int beginning, int end) {

ca533a296a8a76a61c36fac671b254f91b05a00c
poleralph@gmail.com <ralfs.stabins2006@gmail.com>
2024-11-13 10:33:44 +0200

    Payment Card (6 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -15,22 +15,22 @@ public class App
         paulsCard.eatHeartily();
         mattsCard.eatAffordably();
 
-        System.out.println(paulsCard);
-        System.out.println(mattsCard);
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
         
         paulsCard.addMoney(20);
         mattsCard.eatHeartily();
         
-        System.out.println(paulsCard);
-        System.out.println(mattsCard);
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
         
         paulsCard.eatAffordably();
         paulsCard.eatAffordably();
         
         mattsCard.addMoney(50);
         
-        System.out.println(paulsCard);
-        System.out.println(mattsCard);
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
 
 
 
